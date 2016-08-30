3ad8dcb80500e6732a90dbc46fbc3a7d1b94b5c0
Gritsenko Ivan <vanvector@gmail.com>
2016-08-30 18:10:49 +0300

    Repository Init.

---
+++ Payment/Models/CreditCardViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Payment.Models
+{
+    public class CreditCardViewModel
+    {
+        [CreditCard]
+        [Required]
+        public string Number { get; set; }
+        [RegularExpression("([0-9]+)", ErrorMessage = "The field {0} is incorrect.")]
+        [Required]
+        public int CVC { get; set; }
+        [RegularExpression("([0-9]{1,2})", ErrorMessage = "The field {0} is incorrect.")]
+        [Required]
+        [DisplayName("Expiration Month")]
+        public int ExiprationMonth { get; set; }
+        [RegularExpression("([0-9]{4})", ErrorMessage = "The field {0} is incorrect.")]
+        [Required]
+        [DisplayName("Expiration Year")]
+        public int ExpirationYear { get; set; }
+    }
+}

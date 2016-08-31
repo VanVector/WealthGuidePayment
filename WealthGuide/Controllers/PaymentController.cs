cc1f7dddf8922273a7465564a5d050ed3f9a1dfb
Gritsenko Ivan <vanvector@gmail.com>
2016-08-31 03:28:16 +0300

    Other files added.

---
+++ WealthGuide/Controllers/PaymentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Payment.Models;
+
+namespace WealthGuide.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public const string CreditCardViewModelKey = "CreditCardViewModelKey";
+
+        public CreditCardViewModel SessionCreditCardViewModel
+        {
+            get { return HttpContext.Session[CreditCardViewModelKey] as CreditCardViewModel; }
+            set { HttpContext.Session[CreditCardViewModelKey] = value; }
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData[CreditCardViewModelKey] = SessionCreditCardViewModel;
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPost(CreditCardViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData[CreditCardViewModelKey] = SessionCreditCardViewModel;
+                return View(viewModel);
+            }
+            SessionCreditCardViewModel = viewModel;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WealthGuide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WealthGuide.Startup))]
+namespace WealthGuide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
